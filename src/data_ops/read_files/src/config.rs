7308191543a651fee142855624d009206bc361e4
Wojtek <woj.lass@protonmail.com>
2021-10-13 01:24:48 +0200

    added auth and config read

--- src/data_ops/read_files/src/config.rs
+++ src/data_ops/read_files/src/config.rs
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2021 Authors <https://github.com/lasswoj/FractalChanneling/blob/main/Authors.md>
  * 
- * Created Date: Tuesday, October 5th 2021, 10:36:01 pm
+ * Created Date: Tuesday, October 5th 2021, 10:35:54 pm
  * Author: Authors
  * 
  *  Copyright (C) 1991, 1992, 1993 Free Software Foundation, Inc.
@@ -22,4 +22,39 @@
  * Modified by the Fractal Team and others 2021.  See the Authors.md
  * file for a list of people on the Fractal Team.
  */
-pub mod toml;
+use serde::Deserialize;
+use std::fs;
+use std::str::FromStr;
+use toml::de::Error;
+
+#[derive(Deserialize, Debug)]
+pub struct Profile {
+    pub name: String,
+    pub password: String,
+}
+
+pub type ParseError = Error;
+// pub type ParseError = Infallible;
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub profile: Option<Profile>,
+}
+
+impl FromStr for Config {
+    type Err = ParseError;
+    fn from_str(input: &str) -> Result<Self, ParseError> {
+        return toml::from_str(input);
+    }
+
+}
+
+
+impl Config{
+    pub fn from_file(filename: &str)-> Result<Self, ParseError>{
+        let data = fs::read_to_string(filename).expect("Unable to read file");
+        let ret = Config::from_str(&*data);
+        return ret;
+    }
+}
+
